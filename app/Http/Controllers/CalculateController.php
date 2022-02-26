d45a7ad38b6d2c23478d25e2f0049d7f2084c25c
Masoumeh <masoumeh@Masoumehs-MacBook-Air.local>
2022-02-26 19:49:35 +0100

    Rework on Controller

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class CalculateController extends Controller
 {
@@ -10,20 +11,51 @@ class CalculateController extends Controller
     public function index(Request $request)
     {
         // Validate all users's inputs
-        
         $validator = Validator::make($request->all(), [
-            'first_number' => 'required|number|max:9|min:1',
-            'second_number' => 'required|number|max:9|min:1',
+            'first_number' => 'required|integer|max:9|min:1',
+            'second_number' => 'required|integer|max:9|min:1',
         ]);
  
         if ($validator->fails()) {
-            return redirect('welcome')
+            return redirect('/')
                         ->withErrors($validator)
                         ->withInput();
         }
  
         // Retrieve the validated input...
         $validated = $validator->validated();
+        $result = array();
+        
+        for ($i = 1; $i <= 100; $i++) {
+            $a = [array($i, $validated['first_number']), array($i, $validated['second_number']), array($i, $validated['first_number']*$validated['second_number']) ];
+        $b = array($this->mul($a[0]), $this->mul($a[1]), $this->mul($a[2]));
+       
+            if(! in_array(true, $b) )
+            array_push($result, $i);
+            else
+            switch ($b) {
+                case array(true, false, false):
+                    array_push($result, 'Frizz');
+                    break;
+                case array(false, true, false):
+                    array_push($result, 'Buzz');
+                    break;
+                case array(true, true, true):
+                    array_push($result, 'FrizzBuzz');
+                    break;
+            }
+
+        }
+   
+        return view('welcome', ['result' => $result]);
     }
+    public function mul(array $number)
+    {
+        if( $number[0] % $number[1] == 0)
+        return true;
+        else
+        return false;
+    }   
+
 
 }
