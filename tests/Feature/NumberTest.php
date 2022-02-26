d0035ea3c6e692ed4c606a180f71e1987904ae42
Masoumeh <masoumeh@Masoumehs-MacBook-Air.local>
2022-02-26 22:34:41 +0100

    Add more test for wrong data

--- tests/Feature/NumberTest.php
+++ tests/Feature/NumberTest.php
@@ -13,7 +13,7 @@ class NumberTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_correct_data()
     {
         $response = $this->call('POST', '/calculate', ["first_number" => 3, "second_number" => 5]);
 
@@ -21,5 +21,18 @@ class NumberTest extends TestCase
         $response->assertViewHasAll([
             'result' => [1,2,"Frizz",4,"Buzz","Frizz",7,8,"Frizz","Buzz",11,"Frizz",13,14,"FrizzBuzz",16,17,"Frizz",19,"Buzz","Frizz",22,23,"Frizz","Buzz",26,"Frizz",28,29,"FrizzBuzz",31,32,"Frizz",34,"Buzz","Frizz",37,38,"Frizz","Buzz",41,"Frizz",43,44,"FrizzBuzz",46,47,"Frizz",49,"Buzz","Frizz",52,53,"Frizz","Buzz",56,"Frizz",58,59,"FrizzBuzz",61,62,"Frizz",64,"Buzz","Frizz",67,68,"Frizz","Buzz",71,"Frizz",73,74,"FrizzBuzz",76,77,"Frizz",79,"Buzz","Frizz",82,83,"Frizz","Buzz",86,"Frizz",88,89,"FrizzBuzz",91,92,"Frizz",94,"Buzz","Frizz",97,98,"Frizz","Buzz"]
         ]);
+    }
+    public function test_wrong_data()
+    {
+        $response = $this->call('POST', '/calculate', ["first_number" => 13, "second_number" => 5]);
+
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors([
+            'second_number'
+        ]);
+        $response->assertSessionHasErrors([
+            'first_number'
+        ]);
+       
     }
 }
